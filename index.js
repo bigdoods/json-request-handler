acb21bb3b61604f788d8a79800570b72638ee0e3
Kai Davenport <kaiyadavenport@gmail.com>
2016-01-24 19:38:44 +0000

    initial tests passing

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// empty
+var concat = require('concat-stream')
+
+module.exports = function(handlerfunction, errorfn){
+  return function(req, res, opts, cb){
+      
+    req.pipe(concat(function(body){
+
+      try {
+        body = JSON.parse(body.toString())
+      } catch (e){
+        if(errorfn){
+          errorfn(e)
+        }
+        else
+        {
+          res.statusCode = 500
+          res.end(e.toString())
+        }
+        return
+      }
+
+      req.jsonBody = body
+
+      handlerfunction(req, res)
+      
+    }))
+  }
+}
