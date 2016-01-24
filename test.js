837af21120ab7cbf3553cfe0037132d798a78187
Kai Davenport <kaiyadavenport@gmail.com>
2016-01-24 19:57:31 +0000

    failing test for manual error handler

--- test.js
+++ test.js
@@ -23,7 +23,7 @@ tape('processes a JSON body', function (t) {
 
       var handler = jsonRequest(function(req, res){
 
-        
+
 
         collectedJSON = req.jsonBody
         res.end('ok')
@@ -173,6 +173,67 @@ tape('handles an error via response', function (t) {
       from(testJSONString).pipe(req).pipe(concat(function(data){
 
         t.equal(data.toString(), 'SyntaxError: Unexpected number')
+        
+        next()
+      }))
+    },
+
+    function(next){
+      testServer.close(next)
+    }
+
+  ], function(err){
+    if(err){
+      t.error(err)
+      t.end()
+      return
+    }
+    t.end()
+  })
+})
+
+tape('handles an error via response', function (t) {
+
+  var testServer = null
+  var collectedJSON = null
+  var collectedRes = null
+  var testJSONString = '{"fruit":"apples", "val":45 8}'
+
+  async.series([
+
+    function(next){
+
+      var handler = jsonRequest(function(req, res){
+        t.error('the handler should not be called')
+        res.end('fail')
+      }, function(err, req, res){
+        res.statusCode = 500
+        res.end(err + ' = the error')
+      })
+
+      testServer = http.createServer(handler)
+
+      testServer.listen(8089, next)
+    },
+
+    function(next){
+      setTimeout(next, 100)
+    },
+
+    function(next){
+
+      
+      var req = hyperquest('http://127.0.0.1:8089', {
+        method:'POST'
+      })
+
+      req.on('response', function(res){
+        t.equal(res.statusCode, 500)
+      })
+
+      from(testJSONString).pipe(req).pipe(concat(function(data){
+
+        t.equal(data.toString(), 'SyntaxError: Unexpected number = the error')
         
         next()
       }))
